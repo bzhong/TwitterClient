dd4704f4ea31d7f3ebcec761af2532e6201406b1
Qianqian Zhong <qianqian.zhong@airbnb.com>
2016-11-05 23:36:56 -0700

    add oauth login, read tweets

---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TimelineActivity.java
@@ -0,0 +1,58 @@
+package com.codepath.apps.mysimpletweets;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.codepath.apps.mysimpletweets.adapters.TweetsArrayAdapter;
+import com.codepath.apps.mysimpletweets.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class TimelineActivity extends AppCompatActivity {
+
+    private TwitterClient client;
+    private ArrayList<Tweet> tweets;
+    private TweetsArrayAdapter adapter;
+    private ListView lvTweets;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        lvTweets = (ListView) findViewById(R.id.lvTweets);
+        tweets = new ArrayList<>();
+        adapter = new TweetsArrayAdapter(this, tweets);
+        lvTweets.setAdapter(adapter);
+
+        client = TwitterApplication.getRestClient();
+        populateTimeline();
+    }
+
+    private void populateTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                Log.d("DEBUG", response.toString());
+                adapter.addAll(Tweet.fromJsonArray(response));
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("DEBUG", errorResponse.toString());
+            }
+        });
+    }
+
+}
